603db1cfdd2630c72361cd6637dbf2138ddaa7ee
norman <norman.vourlat@powershop.co.nz>
2017-03-29 10:22:44 +1300

    Check if public path before access rights

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function checkUser(req, res, next) {
   // /_doorman requests never get proxied
   if(req.url.indexOf('/_doorman') == 0) { return next(); }
 
-  if(userCanAccess(req) || isPublicPath(req)) {
+  if(isPublicPath(req) || userCanAccess(req)) {
     proxyMiddleware(req, res, next);
   } else {
     if(req.session && req.session.auth) {
